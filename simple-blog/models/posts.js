019499ae31124cd63a47d1f07a4cb7042890b30d
Edgardo <medgardo@gmail.com>
2017-10-04 18:37:52 -0400

    Update sequelize model from v3 to v4 format

--- simple-blog/models/posts.js
+++ simple-blog/models/posts.js
@@ -2,12 +2,11 @@ module.exports = (sequelize, DataTypes) => {
   var Posts = sequelize.define('Posts', {
     post: DataTypes.TEXT,
     author: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
   });
+
+  Posts.associate = (models) => {
+    // associations can be defined here
+  }
+
   return Posts;
 };
